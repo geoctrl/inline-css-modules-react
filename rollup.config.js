14bc67e3a0d27dd4dc573231f299fe75a91426a9
Tony Lefler <tony.lefler@canopytax.com>
2023-03-06 16:58:30 -0700

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: "src/use-css-modules",
+  output: {
+    file: "public/bundle.js",
+    format: "es",
+  },
+};
---
+++ src/use-css-modules.js
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+const separator = "||CSS_MODULES||";
+
+const styleRef = {};
+
+export function useCssModules(cssModuleObject) {
+  if (
+    typeof cssModuleObject !== "object" ||
+    !cssModuleObject.styles ||
+    !cssModuleObject.id
+  ) {
+    throw new Error(
+      `[useCssModules] requires an object with structure: { styles: string; id: string; }.
+      Ensure babel plugin "babel-plugin-css-to-module" is properly installed and configured, and the template literal tag "cssModules" is set.
+      Read more:
+      https://github.com/geoctrl/babel-plugin-css-to-modules`
+    );
+  }
+
+  const { styles, id } = cssModuleObject;
+  useEffect(() => {
+    if (styleRef[id]) {
+      styleRef[id].instances += 1;
+    } else {
+      const styleElement = document.createElement("style");
+      styleElement.setAttribute("type", "text/css");
+      styleElement.textContent = styles;
+      styleRef[id] = { instances: 1, element: styleElement };
+      document.head.appendChild(styleElement);
+    }
+
+    return () => {
+      if (styleRef[id].instances === 1) {
+        styleRef[id].element.remove();
+        delete styleRef[id];
+      } else {
+        styleRef[id].instances -= 1;
+      }
+    };
+  }, [cssModuleObject.styles]);
+}
+
+export const cssModules = (strings, ...args) => {
+  const evalString = strings
+    .map((item, i) => {
+      return `${item}${args[i] || ""}`;
+    })
+    .join("");
+
+  const [id, classNameObject, styles] = evalString.split(separator);
+  if (id && classNameObject && styles) {
+    const classNames = JSON.parse(classNameObject);
+    return { style: classNames, s: classNames, css: { styles, id } };
+  }
+  return evalString;
+};
